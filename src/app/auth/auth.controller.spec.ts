e5f71ab4b677c64eb83b7b2e116493bf27620bcb
OlehBabyak <babyakwork@gmail.com>
2024-11-26 18:40:32 +0200

    tests

--- src/app/auth/auth.controller.spec.ts
+++ src/app/auth/auth.controller.spec.ts
@@ -1,18 +1,130 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { RefreshDTO, SignInDTO } from './DTO/sign-in.dto';
+import { BadRequestException, ForbiddenException } from '@nestjs/common';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
+
+  // Mock the AuthService
+  const mockAuthService = {
+    signIn: jest.fn(),
+    refreshTokens: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should return user and tokens on success', async () => {
+      const signInDto: SignInDTO = {
+        wallet: '0x123',
+        nonce: 'some-nonce',
+        signature: 'valid-signature',
+      };
+
+      const mockResponse = {
+        user: { wallet: signInDto.wallet },
+        tokens: {
+          accessToken: 'access-token',
+          refreshToken: 'refresh-token',
+        },
+      };
+
+      // Mock the service method to return the mock response
+      mockAuthService.signIn.mockResolvedValue(mockResponse);
+
+      const result = await controller.signIn(signInDto);
+
+      expect(result).toEqual(mockResponse);
+      expect(authService.signIn).toHaveBeenCalledWith(signInDto);
+    });
+
+    it('should throw BadRequestException if wallet addresses do not match', async () => {
+      const signInDto: SignInDTO = {
+        wallet: '0x123',
+        nonce: 'some-nonce',
+        signature: 'invalid-signature',
+      };
+
+      // Simulate the error from service
+      mockAuthService.signIn.mockRejectedValue(
+        new BadRequestException("Wallet addresses doesn't match"),
+      );
+
+      await expect(controller.signIn(signInDto)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
+
+    it('should throw BadRequestException if nonce is invalid', async () => {
+      const signInDto: SignInDTO = {
+        wallet: '0x123',
+        nonce: 'invalid-nonce',
+        signature: 'valid-signature',
+      };
+
+      // Simulate the error from service
+      mockAuthService.signIn.mockRejectedValue(
+        new BadRequestException('Nonce mismatch'),
+      );
+
+      await expect(controller.signIn(signInDto)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
+  });
+
+  describe('refresh', () => {
+    it('should return new pair of jwt tokens on success', async () => {
+      const refreshDTO: RefreshDTO = {
+        wallet: '0x123',
+        refreshToken: 'refresh-token',
+      };
+
+      const mockResponse = {
+        tokens: {
+          accessToken: 'new-access-token',
+          refreshToken: 'new-refresh-token',
+        },
+      };
+
+      // Mock the service method to return the mock response
+      mockAuthService.refreshTokens.mockResolvedValue(mockResponse);
+
+      const result = await controller.refresh(refreshDTO);
+
+      expect(result).toEqual(mockResponse);
+      expect(authService.refreshTokens).toHaveBeenCalledWith(refreshDTO);
+    });
+
+    it('should throw ForbiddenException if refresh token was expired', async () => {
+      const refreshDTO: RefreshDTO = {
+        wallet: '0x123',
+        refreshToken: 'expired-refresh-token',
+      };
+
+      // Simulate the error from service
+      mockAuthService.refreshTokens.mockImplementation(async () => {
+        throw new ForbiddenException('Refresh token has expired.');
+      });
+
+      await expect(controller.refresh(refreshDTO)).rejects.toThrow(
+        new ForbiddenException('Refresh token has expired.'),
+      );
+    });
   });
 });
--- src/app/user/user.controller.spec.ts
+++ src/app/user/user.controller.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { RedisProvider } from 'src/providers/cache/redis.provider';
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: UserService;
+
+  // Mock RedisProvider and UserService
+  const mockRedisProvider = {
+    getCachedValue: jest.fn().mockResolvedValue('mocked-nonce'),
+    buildCacheKey: jest.fn().mockReturnValue('mocked-key'),
+    hashIdentifiers: jest.fn().mockReturnValue('mocked-hash'),
+  };
+
+  const mockUserService = {
+    generateNonce: jest.fn().mockResolvedValue('mocked-nonce'),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        { provide: UserService, useValue: mockUserService },
+        { provide: RedisProvider, useValue: mockRedisProvider },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should generate nonce successfully', async () => {
+    const wallet = 'test-wallet';
+    const response = await controller.generateNonce({ wallet });
+
+    expect(response).toEqual({ nonce: 'mocked-nonce' });
+
+    expect(userService.generateNonce).toHaveBeenCalledWith(wallet);
   });
 });
