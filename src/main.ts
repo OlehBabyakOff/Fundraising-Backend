efb2292cf8890d0c283da5f2dfbc4d8c80f9283f
OlehBabyak <babyakwork@gmail.com>
2024-11-27 01:59:32 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   const configService = app.get<ConfigService>(ConfigService);
 
   await app.listen(configService.get<number>('PORT'));
