89a94af97f7bd737efc1e2b4c7394950df188c11
OlehBabyak <babyakwork@gmail.com>
2024-11-26 18:59:14 +0200

    ethers provider update

--- src/config/web3/index.ts
+++ src/config/web3/index.ts
@@ -4,6 +4,7 @@ export const EthersConfig = () => ({
     RPC_URL:
       process.env.INFURA_RPC_URL ||
       'https://sepolia.infura.io/v3/384a472d6b5d4447b7a9f86afcc15635',
+    FACTORY_ADDRESS: process.env.FACTORY_ADDRESS || '',
   },
   PINATA: {
     API_KEY: process.env.PINATA_API_KEY || '',
